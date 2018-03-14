3eb9d109b168f18f0579a00cac9b259716ba858e
Baalateja Kataru <kavesbteja@gmail.com>
2018-03-14 17:47:55 +0000

    Initial framework complete

---
+++ static/js/custom.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    $('.parallax').parallax();
+});
+$('a[href^="#"]').on('click', function(e) {
+    e.preventDefault();
+
+    var target = this.hash;
+    if (target != "#questions1" && target != "#questions2" && target != "#questions3" && target != "#dates" && target != "#terms") {
+        var $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top - 75
+        }, 900, 'swing', function() {
+            window.location.hash = target;
+        });
+    }
+
+});
